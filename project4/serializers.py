5c79e990773cbd7830d1a0a4e7c911d273e27ae2
Kumush <xurshidazaynuddinova@gmail.com>
2026-07-15 11:08:01 +0300

    2

---
+++ project4/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Product
+
+class ProductSerializers(ModelSerializer):
+    class Meta:
+        model=Product
+        fields = ["id", "name", "created_at", "price", "descriptions"]
+        read_only_fields=["id"]
